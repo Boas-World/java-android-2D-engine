b1f79a2f48feb0c3ed92855eba357afed7410ff5
Boas-World <84286574+Boas-World@users.noreply.github.com>
2021-10-13 17:00:44 +0200

    Added constructor

--- com/java_engine/Engine.java
+++ com/java_engine/Engine.java
@@ -13,6 +13,14 @@ Public class Engine{
     static int wbX_1;
     static int wbY_2;
 
+    public Engine(x1,y1,x2,y2){
+
+        wbX_1=x1;
+        wbY_1=y1;
+        wbX_2=x2;
+        wbY_2=y2;
+
+    }
 
     public static void main(String[] args){
         
