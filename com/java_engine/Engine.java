8ed2e28cc1d6c9931074158108a550728ad3b45d
Boas-World <84286574+Boas-World@users.noreply.github.com>
2021-10-13 16:41:42 +0200

    Added some variables

--- com/java_engine/Engine.java
+++ com/java_engine/Engine.java
@@ -1,12 +1,25 @@
 package com.java_engine;
 
 import java.io.*;
+import java.util.*;
 
 Public class Engine{
+    
+    static ArrayList<Integer> posX=new ArrayList<>();
+    static ArrayList<Integer> posY=new ArrayList<>();
+    
+    static int wbX_1;
+    static int wbY_1;
+    static int wbX_1;
+    static int wbY_2;
+
 
     public static void main(String[] args){
-
+        
+        
+        
     }
+    
     
       
 }
